132b3ec169dc3190b8d26d3e49c66e03a4365964
rakibprog <contact.engrakib@gmail.com>
2024-02-27 12:26:06 +0600

    added toogle settings

--- includes/modules/HeadingGradient/HeadingGradient.php
+++ includes/modules/HeadingGradient/HeadingGradient.php
@@ -13,13 +13,31 @@ class INFTNC_HeadingGradient extends ET_Builder_Module {
 
 	public function init() {
 		$this->name = esc_html__( 'Heading Gradient - Infinity TNC', 'inftnc-infinity-tnc-divi-modules' );
+
+        // Toggle settings
+		$this->settings_modal_toggles  = array(
+			
+			'general'  => array(
+				'toggles' => array(
+					'main_content' => esc_html__( 'Title Text', 'inftnc-infinity-tnc-divi-modules' ),
+				),
+			),
+			'advanced' => array(
+				'toggles' => array(
+					'gradient'   => array(
+						'title' => esc_html__( 'Gradient Style', 'inftnc-infinity-tnc-divi-modules' ),
+						'priority' => 50,
+					),
+				),
+			),
+		);
 	}
 
 	public function get_fields() {
 		return array(
 			'content' => array(
 				'label'           => esc_html__( 'Content', 'inftnc-infinity-tnc-divi-modules' ),
-				'type'            => 'tiny_mce',
+				'type'            => 'textarea',
 				'option_category' => 'basic_option',
 				'description'     => esc_html__( 'Content entered here will appear inside the module.', 'inftnc-infinity-tnc-divi-modules' ),
 				'toggle_slug'     => 'main_content',
