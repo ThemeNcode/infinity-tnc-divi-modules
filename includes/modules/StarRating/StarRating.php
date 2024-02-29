ac5949902feac856696aca10450055251183eb93
rakibprog <contact.engrakib@gmail.com>
2024-02-29 12:49:29 +0600

    added star rating fields

--- includes/modules/StarRating/StarRating.php
+++ includes/modules/StarRating/StarRating.php
@@ -24,13 +24,80 @@ class INFTNC_StarRating extends ET_Builder_Module {
 			'advanced' => array(
 				'toggles' => array(
 					'rating'   => array(
-						'title' => esc_html__( 'Rating Style', 'inftnc-infinity-tnc-divi-modules' ),
+						'title' => esc_html__( 'Star Style', 'inftnc-infinity-tnc-divi-modules' ),
 						'priority' => 50,
+					),
+					'title'   => array(
+						'title' => esc_html__( 'Title Text', 'inftnc-infinity-tnc-divi-modules' ),
+						'priority' => 51,
+					),
+					'rating_text'   => array(
+						'title' => esc_html__( 'Rating Text', 'inftnc-infinity-tnc-divi-modules' ),
+						'priority' => 52,
 					),
 				),
 			),
 		);
 	}
+
+	/**
+	 * Module's advanced fields configuration
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return array
+	 */
+	function get_advanced_fields_config() { 
+		return array(
+			'fonts'           => array(
+				'title' => array(
+					'label'          => esc_html__( 'Title Text','inftnc-infinity-tnc-divi-modules' ),
+					'css'            => array(
+						'main' => [
+							'%%order_class%%',
+						],
+					),
+
+					'font_size'      => array(
+						'default' => '30px',
+					),
+
+					'line_height'    => array(
+						'default' => '1em',
+					),
+
+					'letter_spacing' => array(
+						'default' => '0px',
+					),
+				),
+
+				'rating_number' => array(
+					'label'          => esc_html__( 'Rating Number','inftnc-infinity-tnc-divi-modules' ),
+					'css'            => array(
+						'main' => [
+							'%%order_class%%',
+						],
+					),
+
+					'font_size'      => array(
+						'default' => '30px',
+					),
+
+					'line_height'    => array(
+						'default' => '1em',
+					),
+
+					'letter_spacing' => array(
+						'default' => '0px',
+					),
+				),
+			),
+
+			'text'            => false,
+			
+		);
+		
+   }
 
 	public function get_fields() {
 		return array(
@@ -44,9 +111,76 @@ class INFTNC_StarRating extends ET_Builder_Module {
 				),
 				'toggle_slug'     => 'main_content',
 			),
-			
-			'rating' => array(
+
+			'rating_five' => array(
 				'label'           => esc_html__( 'Rating', 'inftnc-infinity-tnc-divi-modules' ),
+				'type'            => 'range',
+                'default'         => 5,
+                'range_settings' => array(
+					'min'  => 0,
+					'max'  => 5,
+					'step' => .1,
+				),
+				'toggle_slug'     => 'main_content',
+				'show_if'		  => array(
+					'rating_scale'  => '0-5',
+				),
+			),
+
+			'rating_ten' => array(
+				'label'           => esc_html__( 'Rating', 'inftnc-infinity-tnc-divi-modules' ),
+				'type'            => 'range',
+                'default'         => 10,
+                'range_settings' => array(
+					'min'  => 0,
+					'max'  => 10,
+					'step' => .1,
+				),
+				'toggle_slug'     => 'main_content',
+				'show_if'		  => array(
+					'rating_scale'  => '0-10',
+				),
+			),
+
+			'title' => array(
+				'label'           => esc_html__( 'Title', 'inftnc-infinity-tnc-divi-modules' ),
+				'type'            => 'text',
+				'toggle_slug'     => 'main_content',
+			),
+
+			'show_rating_number' => array(
+				'label'             => esc_html__( 'Rating Number', 'infinity' ),
+				'type'              => 'yes_no_button',
+				'default'			=> 'off',
+				'options'           => array(
+					'on'  => esc_html__( 'Show', 'inftnc-infinity-tnc-divi-modules' ),
+					'off' => esc_html__( 'Hide', 'inftnc-infinity-tnc-divi-modules' ),
+				),
+				'toggle_slug'     => 'main_content',
+			),
+
+			'color' => array(
+				'label'           => esc_html__( 'Star Icon Color', 'inftnc-infinity-tnc-divi-modules' ),
+				'type'            => 'color',
+				'tab_slug'        => 'advanced',
+				'toggle_slug'     => 'rating',
+			),
+
+			'star_size' => array(
+				'label'           => esc_html__( 'Star Size', 'inftnc-infinity-tnc-divi-modules' ),
+				'type'            => 'range',
+                'default'         => 0,
+                'range_settings' => array(
+					'min'  => 0,
+					'max'  => 100,
+					'step' => 1,
+				),
+				'tab_slug'        => 'advanced',
+				'toggle_slug'     => 'rating',
+			),
+
+			'gap' => array(
+				'label'           => esc_html__( 'Star Gap', 'inftnc-infinity-tnc-divi-modules' ),
 				'type'            => 'range',
                 'default'         => 0,
                 'range_settings' => array(
@@ -54,8 +188,12 @@ class INFTNC_StarRating extends ET_Builder_Module {
 					'max'  => 10,
 					'step' => .1,
 				),
-				'toggle_slug'     => 'main_content',
+				'toggle_slug'     => 'rating',
+				'tab_slug'		  => 'advanced',
 			),
+
+			
+
 		);
 	}
 
