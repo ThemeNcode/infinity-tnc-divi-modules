ef6e4697af7cb257242f3d7ede798de6b618c884
rakibprog <contact.engrakib@gmail.com>
2024-03-06 11:54:38 +0600

    added map zomm level

--- includes/modules/EmbedMap/EmbedMap.php
+++ includes/modules/EmbedMap/EmbedMap.php
@@ -42,7 +42,7 @@ class INFTNC_EmbedMap extends ET_Builder_Module {
 			'source_type' => array(
 				'label'           => esc_html__( 'Source Type', 'inftnc-infinity-tnc-divi-modules' ),
 				'type'            => 'select',
-				'default'	      => 'emebed_code',
+				'default'	      => 'latitude_longitude',
 				'options'         => array(
 					'emebed_code'           	 	=> esc_html__( 'Embed Code', 'inftnc-infinity-tnc-divi-modules' ),
 					'latitude_longitude'            => esc_html__( 'Latitude & Longitude', 'inftnc-infinity-tnc-divi-modules' ),
@@ -63,7 +63,6 @@ class INFTNC_EmbedMap extends ET_Builder_Module {
 					
 				),
 			),
-
 
 			'openstreetmap_latitude' => array(
 				'label'           => esc_html__( 'Latitude', 'inftnc-infinity-tnc-divi-modules' ),
@@ -90,10 +89,17 @@ class INFTNC_EmbedMap extends ET_Builder_Module {
 			),
 
 			'map_width' => array(
-				'label'           => esc_html__( 'Width', 'dicm-divi-custom-modules' ),
-				'type'            => 'text',
-				'option_category' => 'basic_option',
-				'description'     => esc_html__( 'Text entered here will appear as title.', 'dicm-divi-custom-modules' ),
+				'label'           => esc_html__( 'Width', 'inftnc-infinity-tnc-divi-modules' ),
+				'type'            => 'range',
+				'allowed_units'    => array( '%', 'em', 'rem', 'px', 'cm', 'mm', 'in', 'pt', 'pc', 'ex', 'vh', 'vw' ),
+				'default'          => '100',
+				'default_unit'     => '%',
+				'default_on_front' => '100%',
+                'range_settings' => array(
+					'min'  => 1,
+					'max'  => 100,
+					'step' => 1,
+				),
 				'toggle_slug'     => 'main_content',
 				'show_if'         => array(
 					'source_type' => 'latitude_longitude',
@@ -101,16 +107,37 @@ class INFTNC_EmbedMap extends ET_Builder_Module {
 			),
 
 			'map_height' => array(
-				'label'           => esc_html__( 'Height', 'dicm-divi-custom-modules' ),
-				'type'            => 'text',
-				'option_category' => 'basic_option',
-				'description'     => esc_html__( 'Text entered here will appear as title.', 'dicm-divi-custom-modules' ),
+				'label'           => esc_html__( 'Width', 'inftnc-infinity-tnc-divi-modules' ),
+				'type'            => 'range',
+				'allowed_units'    => array( '%', 'em', 'rem', 'px', 'cm', 'mm', 'in', 'pt', 'pc', 'ex', 'vh', 'vw' ),
+				'default'          => '500',
+				'default_unit'     => 'px',
+				'default_on_front' => '500px',
+                'range_settings' => array(
+					'min'  => 1,
+					'max'  => 100,
+					'step' => 1,
+				),
 				'toggle_slug'     => 'main_content',
 				'show_if'         => array(
 					'source_type' => 'latitude_longitude',
 				),
 			),
 
+			'map_zoom' => array(
+				'label'           => esc_html__( 'Zoom', 'inftnc-infinity-tnc-divi-modules' ),
+				'type'            => 'range',
+				'default'          => '14',
+                'range_settings' => array(
+					'min'  => 1,
+					'max'  => 100,
+					'step' => 1,
+				),
+				'toggle_slug'     => 'main_content',
+				'show_if'         => array(
+					'source_type' => 'latitude_longitude',
+				),
+			),
 
 			'embed_code' => array(
 				'label'           => esc_html__( 'Embed Code', 'inftnc-infinity-tnc-divi-modules' ),
@@ -134,6 +161,9 @@ class INFTNC_EmbedMap extends ET_Builder_Module {
 		$embed_code                  = $this->props['embed_code'];
 		$openstreetmap_longtitude    = $this->props['openstreetmap_longtitude'];
 		$openstreetmap_latitude      = $this->props['openstreetmap_latitude'];
+		$width 						 = $this->props['map_width'];
+		$height						 = $this->props['map_height'];
+		$zoom 						 = $this->props['map_zoom'];
 
 		$geopointA 		= new GeoPoint(23.812238958241778,90.42481753435429);
 		$boundingBox    =  $geopointA->boundingBox(3, 'miles');
@@ -141,12 +171,15 @@ class INFTNC_EmbedMap extends ET_Builder_Module {
 		$maxLongitude   =  $boundingBox->getMaxLongitude();
 		$minLatitude    =  $boundingBox->getMinLatitude();
 		$minLongitude   =  $boundingBox->getMinLongitude(); 
-	
+		
 		if( 'emebed_code'  === $source_type  && 'google_map' === $map_type ){
 			 $map = sprintf('%1$s', /* 01 */ $embed_code );
 		} elseif ( 'latitude_longitude'  === $source_type  && 'google_map' === $map_type ) { 
-			$map = sprintf('<iframe src = "https://maps.google.com/maps?q=%1$s&hl;z=14&amp;output=embed"></iframe>',
+			$map = sprintf('<iframe src = "https://maps.google.com/maps?q=%1$s&z=%4$s&amp;output=embed" width="%2$s"height="%3$s"></iframe>',
 			 /* 01 */ $latitude_logitude,
+			 /* 02 */ $width,
+			 /* 03 */ $height,
+			 /* 04 */ $zoom,
 			);
 		} elseif ( 'emebed_code'  === $source_type  && 'open_street_map' === $map_type ) {
 
@@ -154,13 +187,15 @@ class INFTNC_EmbedMap extends ET_Builder_Module {
 
 		} elseif ('latitude_longitude'  === $source_type  && 'open_street_map' === $map_type) {
 			
-			$map = sprintf('<iframe src="http://www.openstreetmap.org/export/embed.html?bbox=%1$s,%2$s,%3$s,%4$s&marker=%5$s,%6$s&layers=ND" frameborder="0"></iframe>',
+			$map = sprintf('<iframe src="http://www.openstreetmap.org/export/embed.html?bbox=%.20f,%2$s,%3$s,%4$s&marker=%5$s,%6$s&layers=ND" frameborder="0" width="%7$s" height="%8$s"></iframe>',
 				/* Min Latitude 01 */ $minLatitude,
 				/* Min Longitude 02 */ $minLongitude,
 				/* Max Longitude 03 */ $maxLongitude,
 				/* Max Latitude 04 */ $maxLatitude,
 				/* Marker 05 */ $openstreetmap_longtitude,
-				/* Marker 06 */ $openstreetmap_latitude
+				/* Marker 06 */ $openstreetmap_latitude,
+				/* 07 */ $width,
+			    /* 08 */ $height,
 			);
 		}
 		
@@ -170,3 +205,5 @@ class INFTNC_EmbedMap extends ET_Builder_Module {
 }
 
 new INFTNC_EmbedMap;
+
+
