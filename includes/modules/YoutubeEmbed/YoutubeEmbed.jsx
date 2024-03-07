6b1122fc6173a51e51d709f99acee2735780da42
rakibprog <contact.engrakib@gmail.com>
2024-03-07 17:43:58 +0600

    render youtube iframe

--- includes/modules/YoutubeEmbed/YoutubeEmbed.jsx
+++ includes/modules/YoutubeEmbed/YoutubeEmbed.jsx
@@ -9,19 +9,34 @@ class YoutubeEmbed extends Component {
   static slug = 'inftnc_youtube_embed';
 
   render() {
-    const { video_type, video_method, youtube_url } = this.props;
+    const { video_type, video_method, youtube_url,youtube_id,youtube_embed,video_start,video_end,autoplay,mute,loop,player_control,video_rel} = this.props;
     
+    let autoplayValue;
+     //Autoplay Value
+    ( 'on' === autoplay ) ? ( autoplayValue = 1 ) : ( autoplayValue = 0 );
+		//Mute Value 
+    let muteValue;
+		( 'on' === mute ) ? ( muteValue = 1 ) : ( muteValue = 0 );
+		//Loop Value 
+    let loopValue;
+		( 'on' === loop ) ? ( loopValue = 1 ) : ( loopValue = 0 );
+		//Player Control 
+    let controlValue;
+		( 'on' === player_control  ) ? ( controlValue = 1 ) : ( controlValue = 0 );
+		//Video Rel 
+    let relValue;
+		('on' === video_rel ) ? ( relValue = 1 ) : ( relValue = 0 );
+
+ 
     let inftncYoutubeIframe = null;
 
-    if (video_type === 'video' && video_method === 'video_url' && youtube_url) {
+    if (video_type === 'video' && video_method === 'video_url') {
 
       const videoId = this.getYouTubeVideoId(youtube_url);
-
-      console.log(videoId);
       
       if (videoId) {
 
-        const iframeSrc = `https://www.youtube.com/embed/${videoId}`;
+        const iframeSrc = `https://www.youtube.com/embed/${videoId}?controls=${controlValue}&amp;autoplay=${autoplayValue}&amp;loop=${loopValue}&amp;mute=${muteValue}&amp;start=${video_start}&amp;end=${video_end}&amp;rel=${relValue}`;
 
         inftncYoutubeIframe = (
           <iframe 
@@ -33,6 +48,62 @@ class YoutubeEmbed extends Component {
           ></iframe>
         );
       }
+    } else if ( video_type === 'video' && video_method === 'video_id' ) {
+
+      const iframeSrc = `https://www.youtube.com/embed/${youtube_id}?controls=${controlValue}&amp;autoplay=${autoplayValue}&amp;loop=${loopValue}&amp;mute=${muteValue}&amp;start=${video_start}&amp;end=${video_end}&amp;rel=${relValue}`;
+
+      inftncYoutubeIframe = (
+        <iframe 
+          src={iframeSrc} 
+          title="YouTube video player" 
+          frameBorder="0" 
+          allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+          allowFullScreen
+        ></iframe>
+      );
+
+    } else if ( video_type === 'video' && video_method === 'embed_code' && youtube_url ) {
+
+      inftncYoutubeIframe = <div dangerouslySetInnerHTML={{__html: youtube_embed}}></div>
+
+    } else if (video_type  === 'playlist' && 'video_url' ===  video_method  ) {
+
+      const videoId = this.getYouTubeVideolistId(youtube_url);
+      
+      if (videoId) {
+
+        const iframeSrc = `https://www.youtube.com/embed/videoseries?controls=${controlValue}&amp;autoplay=${autoplayValue}&amp;loop=${loopValue}&amp;mute=${muteValue}&amp;start=${video_start}&amp;end=${video_end}&amp;rel=${relValue}&amp;list=${videoId}`;
+
+        inftncYoutubeIframe = (
+          <iframe 
+            src={iframeSrc} 
+            title="YouTube video player" 
+            frameBorder="0" 
+            allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+            allowFullScreen
+          ></iframe>
+        );
+      }
+
+
+    } else if ( video_type  === 'playlist' && 'video_id'  ===   video_method ) {
+
+      const iframeSrc = `https://www.youtube.com/embed/videoseries?controls=${controlValue}&amp;autoplay=${autoplayValue}&amp;loop=${loopValue}&amp;mute=${muteValue}&amp;start=${video_start}&amp;end=${video_end}&amp;rel=${relValue}&amp;list=${youtube_id}`;
+
+        inftncYoutubeIframe = (
+          <iframe 
+            src={iframeSrc} 
+            title="YouTube video player" 
+            frameBorder="0" 
+            allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+            allowFullScreen
+          ></iframe>
+        );
+
+    }  else if ( video_type  === 'playlist' && 'embed_code'  ===   video_method ) {
+
+       inftncYoutubeIframe = <div dangerouslySetInnerHTML={{__html: youtube_embed}}></div>
+
     }
 
     return (
@@ -45,10 +116,8 @@ class YoutubeEmbed extends Component {
   }
 
   getYouTubeVideoId(url) {
-    console.log(url);
     const regex = /(?:youtube(?:-nocookie)?\.com\/(?:[^\/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^“&?\/ ]{11})/i;
     const match = url.match(regex);
-    console.log(match);
     return match ? match[1] : null;
   }
 
