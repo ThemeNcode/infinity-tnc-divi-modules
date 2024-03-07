2269fa885826eab729151add82158901a5244af1
rakibprog <contact.engrakib@gmail.com>
2024-03-07 15:37:22 +0600

    rende iframe in jsx

--- includes/modules/YoutubeEmbed/YoutubeEmbed.jsx
+++ includes/modules/YoutubeEmbed/YoutubeEmbed.jsx
@@ -10,12 +10,21 @@ class YoutubeEmbed extends Component {
   static slug = 'inftnc_youtube_embed';
 
   render() {
-    const Content = this.props.content;
+
+    const {video_type,video_method,youtube_url,youtube_id,youtube_embed,video_start,video_end,autoplay,mute,loop,player_control,video_rel}  = this.props;
+    
+    let inftncYoutubeIframe;
+
+    if(video_type === 'video' && video_method === 'video_url') {
+         const iframeSrc = `https://www.youtube.com/embed/z8uxAkjll5g`;
+         inftncYoutubeIframe = <iframe src={iframeSrc} title="YouTube video player" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>;
+    }
 
     return (
-      <h1>
-        <Content/>
-      </h1>
+
+        <div className="inftnc_youtube_embed_wrapper">
+            {inftncYoutubeIframe}
+        </div>
     );
   }
 }
