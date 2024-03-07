11a1e9616b7dfb019dbc5d5ad04f45318b72c3be
rakibprog <contact.engrakib@gmail.com>
2024-03-07 16:07:13 +0600

    added youtube video url regex expression pattern

--- includes/modules/YoutubeEmbed/YoutubeEmbed.jsx
+++ includes/modules/YoutubeEmbed/YoutubeEmbed.jsx
@@ -4,37 +4,45 @@ import React, { Component } from 'react';
 // Internal Dependencies
 import './style.css';
 
-
 class YoutubeEmbed extends Component {
 
   static slug = 'inftnc_youtube_embed';
 
   render() {
-
-    const {video_type,video_method,youtube_url,youtube_id,youtube_embed,video_start,video_end,autoplay,mute,loop,player_control,video_rel}  = this.props;
+    const { video_type, video_method, youtube_url } = this.props;
     
     let inftncYoutubeIframe;
 
-    if(video_type === 'video' && video_method === 'video_url') {
-
-        function getYouTubeVideoId(url) {
-            const regex = /^(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})$/;
-            const match = url.match(regex);
-            return match ? match[1] : null;
-        }
-        
-        const videoId = getYouTubeVideoId(youtube_url);
-        
-         const iframeSrc = `https://www.youtube.com/embed/${videoId}`;
-         inftncYoutubeIframe = <iframe src={iframeSrc} title="YouTube video player" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>;
+    if (video_type === 'video' && video_method === 'video_url' && youtube_url) {
+      const videoId = this.getYouTubeVideoId(youtube_url);
+      
+      if (videoId) {
+        const iframeSrc = `https://www.youtube.com/embed/${videoId}`;
+        inftncYoutubeIframe = (
+          <iframe 
+            src={iframeSrc} 
+            title="YouTube video player" 
+            frameBorder="0" 
+            allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+            allowFullScreen
+          ></iframe>
+        );
+      }
     }
 
     return (
 
-        <div className="inftnc_youtube_embed_wrapper">
-            {inftncYoutubeIframe}
-        </div>
+      <div className="inftnc_youtube_embed_wrapper">
+          {inftncYoutubeIframe}
+      </div>
+
     );
+  }
+
+  getYouTubeVideoId(url) {
+    const regex = /^(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})$/;
+    const match = url.match(regex);
+    return match ? match[1] : null;
   }
 }
 
