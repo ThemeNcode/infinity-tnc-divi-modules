3065c6f5eaa7b3a26c5aba09ef07a01177e6ee73
rakibprog <contact.engrakib@gmail.com>
2024-03-11 14:14:34 +0600

    get props value in jsx

--- includes/modules/TypeWriter/TypeWriter.jsx
+++ includes/modules/TypeWriter/TypeWriter.jsx
@@ -4,18 +4,22 @@ import React, { Component } from 'react';
 // Internal Dependencies
 import './style.css';
 
-
 class TypeWriter extends Component {
 
   static slug = 'inftnc_type_writer';
 
   render() {
-    const Content = this.props.content;
+
+    const {before_text,typing_text,after_text,typing_speed,typing_backspeed,pause_for,typing_cursor,typing_loop} = this.props;
 
     return (
-      <h1>
-        <Content/>
-      </h1>
+          <div class="inftnc_typewriter_wrapper">
+              <h1 class="inftnc_typewriter_main_title">
+                  <span class="inftnc_before_text"></span>
+                  <span class="inftnc_typewriter_text"></span>
+                  <span class="inftnc_after_text"></span>
+              </h1>
+          </div>
     );
   }
 }
