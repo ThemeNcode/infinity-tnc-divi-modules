7c582680e3be9c2f436952fd05870caf575d9129
rakibprog <contact.engrakib@gmail.com>
2024-03-21 16:23:46 +0600

    added responsive option

--- public/js/inftnc-slick.js
+++ public/js/inftnc-slick.js
@@ -2,25 +2,28 @@ jQuery(document).ready(function($){
 
     $('.inftnc_carousels_logo_wrapper').each(function() {
 
-        var slidesToShow    = $(this).data('slides-to-show'); 
-        var slidesToScroll  = $(this).data('slide-scroll');
-        var animationSpeed  = $(this).data('animation-speed');
-        var autoplay        = $(this).data('autoplay');
-        var autoplaySpeed   = $(this).data('autoplay-speed');
-        var useNavigation   = $(this).data('navigation');
-        var usePagination   = $(this).data('pagination');
-        var infinite        = $(this).data('infinite');
-        var pauseHover      = $(this).data('pause-hover');
-        var swipe           = $(this).data('swipe');
-        var rtl             = $(this).data('rtl');
-       
-    
+        var slidesToShow            = $(this).data('slides-to-show'); 
+        var slidesToScroll          = $(this).data('slide-scroll');
+        var animationSpeed          = $(this).data('animation-speed');
+        var autoplay                = $(this).data('autoplay');
+        var autoplaySpeed           = $(this).data('autoplay-speed');
+        var useNavigation           = $(this).data('navigation');
+        var usePagination           = $(this).data('pagination');
+        var infinite                = $(this).data('infinite');
+        var pauseHover              = $(this).data('pause-hover');
+        var swipe                   = $(this).data('swipe');
+        var rtl                     = $(this).data('rtl');
+        var slidesToShowTablet      = $(this).data('slide-tablet');
+        var slidesToShowPhone       = $(this).data('slide-phone'); 
+        var slidesToScrolTablet     = $(this).data('scroll-tablet');
+        var slidesToScrollPhone     = $(this).data('scroll-phone');       
+        
         $(this).slick({
             dots: true,
             infinite: false,
             speed: animationSpeed,
             slidesToShow: slidesToShow ? slidesToShow : 3, 
-            slidesToScroll: 1,
+            slidesToScroll: slidesToScroll ? slidesToScroll : 2,
             autoplay: autoplay,
             autoplaySpeed: autoplaySpeed,
             arrows: useNavigation,
@@ -34,21 +37,21 @@ jQuery(document).ready(function($){
                     breakpoint: 1024,
                     settings: {
                         slidesToShow: slidesToShow ? slidesToShow : 3,
-                        slidesToScroll: slidesToScroll ? slidesToScroll: 3,
+                        slidesToScroll: slidesToScroll ? slidesToScroll: 2,
                     }
                 },
                 {
                     breakpoint: 600,
                     settings: {
-                        slidesToShow: slidesToShow ? slidesToShow : 2,
-                        slidesToScroll: 2
+                        slidesToShow:slidesToShowTablet,
+                        slidesToScroll: slidesToScrolTablet ? slidesToScroll: 1,
                     }
                 },
                 {
                     breakpoint: 480,
                     settings: {
-                        slidesToShow: 1,
-                        slidesToScroll: 1
+                        slidesToShow: slidesToShowPhone ? slidesToShow : 1,
+                        slidesToScroll: slidesToScrollPhone ? slidesToScroll: 1,
                     }
                 }
                 // You can unslick at a given breakpoint now by adding:
