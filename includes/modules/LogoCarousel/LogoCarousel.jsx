347fae332b393d37705fa5288d644a36b48242fa
rakibprog <contact.engrakib@gmail.com>
2024-03-26 10:04:24 +0600

    added arrows

--- includes/modules/LogoCarousel/LogoCarousel.jsx
+++ includes/modules/LogoCarousel/LogoCarousel.jsx
@@ -26,6 +26,18 @@ class LogoCarousel extends Component {
 
 //  }
 
+    NextArrow() {
+      return (
+        <button type="button" className="slick-inftnc-arrow slick-prev" data-icon="&#x34;"></button>
+      );
+    }
+
+    PrevArrow() {
+      return (
+        <button type="button" className="slick-inftnc-arrow slick-next" data-icon="&#x35;"></button>
+      );
+    }
+
   render() {
     // Destructure Props 
     const {slides_to_show,slides_to_scroll,animation_speed,autoplay,autoplay_speed,use_navigation,use_pagination,slide_spacing,infinite,pause_on_hover,swipe,rtl,logo_grayscale_default,logo_grayscale_hover,logo_hover,navigation_icon_size,navigation_bg_size,navigation_icon_color,navigation_bg_color,pagination_cmn_dots_size,pagination_active_dots_size,pagination_cmn_dots_color,dots_alignment}   = this.props;
@@ -45,13 +57,7 @@ class LogoCarousel extends Component {
     let rtlValue;
     rtl == 'on' ? rtlValue = true :  rtlValue = false ;
 
-
-
-
-  
-  
     var settings = { 
-     
       speed: parseInt(animation_speed),
       slidesToShow: slides_to_show ? parseInt(slides_to_show) : 3,
       slidesToScroll: slides_to_scroll ? parseInt(slides_to_scroll) : 1,
@@ -63,8 +69,8 @@ class LogoCarousel extends Component {
       rtl:rtlValue,
       swipe: swipeValue,
       pauseOnHover: pauseHoverValue,
-      //prevArrow: '<button type="button" className="slick-inftnc-arrow slick-prev" data-icon="&#x34;"></button>',
-      //nextArrow: '<button type="button" className="slick-inftnc-arrow slick-next" data-icon="&#x35;"></button>',
+      prevArrow: <this.NextArrow></this.NextArrow>,
+      nextArrow: <this.PrevArrow></this.PrevArrow>,
       responsive: [
         {
           breakpoint: 1024,
