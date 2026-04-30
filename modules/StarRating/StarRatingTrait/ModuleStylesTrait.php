aced1862d4afc79991f9b2fe1186fd7e90eda94a
rakibprog <contact.engrakib@gmail.com>
2026-04-30 15:09:51 +0600

    fix gap styles

--- modules/StarRating/StarRatingTrait/ModuleStylesTrait.php
+++ modules/StarRating/StarRatingTrait/ModuleStylesTrait.php
@@ -14,59 +14,10 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 use ET\Builder\FrontEnd\Module\Style;
 use ET\Builder\Packages\Module\Options\Css\CssStyle;
-use ET\Builder\Packages\Module\Layout\Components\StyleCommon\CommonStyle;
 
 trait ModuleStylesTrait {
 
 	use CustomCssTrait;
-
-	/**
-	 * Generate CSS declaration for star gap.
-	 *
-	 * @since ??
-	 *
-	 * @param array $args Style declaration arguments.
-	 *
-	 * @return string
-	 */
-	public static function gap_style_declaration( $args ): string {
-		$value = $args['attrValue']['innerContent']['desktop']['value']
-			?? $args['attr']['innerContent']['desktop']['value']
-			?? [];
-
-		$gap = intval( $value['gap'] ?? 0 );
-
-		if ( $gap <= 0 ) {
-			return '';
-		}
-
-		return "margin-left: {$gap}px !important;";
-	}
-
-	/**
-	 * Generate CSS declaration for star alignment.
-	 *
-	 * @since ??
-	 *
-	 * @param array $args Style declaration arguments.
-	 *
-	 * @return string
-	 */
-	public static function alignment_style_declaration( $args ): string {
-		$value = $args['attrValue']['innerContent']['desktop']['value']
-			?? $args['attr']['innerContent']['desktop']['value']
-			?? [];
-
-		$alignment_map = [
-			'left'   => 'flex-start',
-			'center' => 'center',
-			'right'  => 'flex-end',
-		];
-
-		$alignment = $value['star_alignment'] ?? 'left';
-
-		return 'justify-content: ' . ( $alignment_map[ $alignment ] ?? 'flex-start' ) . ';';
-	}
 
 	/**
 	 * StarRating module styles callback.
@@ -81,7 +32,19 @@ trait ModuleStylesTrait {
 		$attrs                       = $args['attrs'] ?? [];
 		$elements                    = $args['elements'];
 		$settings                    = $args['settings'] ?? [];
+		$order_class                 = $args['orderClass'] ?? '';
 		$default_printed_style_attrs = $args['defaultPrintedStyleAttrs'] ?? [];
+
+		$data      = $attrs['starRatingData']['innerContent']['desktop']['value'] ?? [];
+		$gap       = intval( $data['gap'] ?? 0 );
+		$alignment = $data['star_alignment'] ?? 'left';
+
+		$alignment_map = [
+			'left'   => 'flex-start',
+			'center' => 'center',
+			'right'  => 'flex-end',
+		];
+		$justify_css = 'justify-content: ' . ( $alignment_map[ $alignment ] ?? 'flex-start' ) . ';';
 
 		Style::add(
 			[
@@ -118,27 +81,51 @@ trait ModuleStylesTrait {
 					),
 
 					// Star gap CSS.
-					CommonStyle::style(
+					CssStyle::style(
 						[
-							'selector'            => "{$args['orderClass']} .jq-star",
-							'attr'                => $attrs['starRatingData'] ?? [],
-							'declarationFunction' => [ self::class, 'gap_style_declaration' ],
+							'selector' => "{$order_class} .jq-star",
+							'attr'     => [
+								'desktop' => [
+									'value' => [
+										'mainElement' => $gap > 0 ? "margin-left: {$gap}px !important;" : '',
+									],
+								],
+							],
+						]
+					),
+
+					// Reset first star gap.
+					CssStyle::style(
+						[
+							'selector' => "{$order_class} .jq-star:first-child",
+							'attr'     => [
+								'desktop' => [
+									'value' => [
+										'mainElement' => $gap > 0 ? 'margin-left: 0 !important;' : '',
+									],
+								],
+							],
 						]
 					),
 
 					// Star alignment CSS.
-					CommonStyle::style(
+					CssStyle::style(
 						[
-							'selector'            => "{$args['orderClass']} .inftnc_rating_inner_wrapper",
-							'attr'                => $attrs['starRatingData'] ?? [],
-							'declarationFunction' => [ self::class, 'alignment_style_declaration' ],
+							'selector' => "{$order_class} .inftnc_rating_inner_wrapper",
+							'attr'     => [
+								'desktop' => [
+									'value' => [
+										'mainElement' => $justify_css,
+									],
+								],
+							],
 						]
 					),
 
 					// Custom CSS.
 					CssStyle::style(
 						[
-							'selector'  => $args['orderClass'],
+							'selector'  => $order_class,
 							'attr'      => $attrs['css'] ?? [],
 							'cssFields' => self::custom_css(),
 						]
